9dfabe0971e829d7c3727edd81a0881dcec31307
Daniel <daniferna@outlook.com>
2018-10-16 18:51:05 +0200

    Implementada la nueva barra de Material Design 2.0 y cambiadas las librerias a las nuevas AndroidX

--- app/src/main/java/es/uniovi/uo257977/clock/AlarmsFragment.java
+++ app/src/main/java/es/uniovi/uo257977/clock/AlarmsFragment.java
@@ -3,10 +3,11 @@ package es.uniovi.uo257977.clock;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 
 /**
--- app/src/main/java/es/uniovi/uo257977/clock/MainActivity.java
+++ app/src/main/java/es/uniovi/uo257977/clock/MainActivity.java
@@ -2,38 +2,29 @@ package es.uniovi.uo257977.clock;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 
-import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
-import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+import com.google.android.material.bottomappbar.BottomAppBar;
 
-import java.util.Arrays;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity implements AlarmsFragment.OnFragmentInteractionListener {
 
-    /*
-    Barra de navegacion de la libreria externa: AHBottomNavigation (Más opciones que la original de Android)
-    https://github.com/aurelhubert/ahbottomnavigation
-    */
-    AHBottomNavigation bottomNavigation;
     Fragment fragmentTest;
+    BottomAppBar bottomAppBar;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AHBottomNavigationItem alarms = new AHBottomNavigationItem(getString(R.string.menu_alarm), R.drawable.ic_alarm_black_24dp);
-        AHBottomNavigationItem timer = new AHBottomNavigationItem(getString(R.string.menu_timer), R.drawable.ic_hourglass_empty_black_24dp);
-        AHBottomNavigationItem stopwatch = new AHBottomNavigationItem(getString(R.string.menu_stopwatch), R.drawable.ic_timer_black_24dp);
-        AHBottomNavigationItem scoreboard = new AHBottomNavigationItem(getString(R.string.menu_scoreboard), R.drawable.ic_sort_black_24dp);
+        bottomAppBar = findViewById(R.id.bar);
+        fragmentTest = getSupportFragmentManager().findFragmentById(R.id.fragment);
 
-        bottomNavigation = findViewById(R.id.bottom_navigation);
-        bottomNavigation.addItems(Arrays.asList(alarms, timer, stopwatch, scoreboard));
-        bottomNavigation.setCurrentItem(0);
-        bottomNavigation.setAccentColor(ContextCompat.getColor(getApplicationContext(), R.color.accent));
+        bottomAppBar.replaceMenu(R.menu.navigation);
+
     }
 
     @Override
