296642fa4f64a2b97118eb5ae4f2f021c4f2fab3
Daniel <daniferna@outlook.com>
2018-10-17 19:25:10 +0200

    Implementada la navegacion entre fragments.

--- app/src/main/java/es/uniovi/uo257977/clock/MainActivity.java
+++ app/src/main/java/es/uniovi/uo257977/clock/MainActivity.java
@@ -1,19 +1,21 @@
 package es.uniovi.uo257977.clock;
 
-import android.net.Uri;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
-public class MainActivity extends AppCompatActivity implements AlarmsFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity {
 
-    Fragment fragmentTest;
     BottomAppBar bottomAppBar;
-    RecyclerView recyclerView;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +23,61 @@ public class MainActivity extends AppCompatActivity implements AlarmsFragment.On
         setContentView(R.layout.activity_main);
 
         bottomAppBar = findViewById(R.id.bar);
-        fragmentTest = getSupportFragmentManager().findFragmentById(R.id.fragment);
 
         bottomAppBar.replaceMenu(R.menu.navigation);
 
-    }
+        context = this;
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-        //TODO No se muy bien que va aqui
+        final ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            private static final int NUM_FRAGMENTS = 4;
+
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    case 0:
+                        // Fragment de la alarma
+                        return Fragment.instantiate(context, AlarmsFragment.class.getName());
+                    case 1:
+                        //TODO Fragment del cronometro
+                        return Fragment.instantiate(context, AlarmsFragment.class.getName());
+                    case 2:
+                        //TODO Fragment del temporizador
+                        return Fragment.instantiate(context, AlarmsFragment.class.getName());
+                    case 3:
+                        //TODO Fragment de la puntacion
+                        return Fragment.instantiate(context, AlarmsFragment.class.getName());
+                    default:
+                        return null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return NUM_FRAGMENTS;
+            }
+        });
+
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_alarms:
+                        viewPager.setCurrentItem(0, true);
+                        break;
+                    case R.id.navigation_stopwatch:
+                        viewPager.setCurrentItem(1, true);
+                        break;
+                    case R.id.navigation_timer:
+                        viewPager.setCurrentItem(2, true);
+                        break;
+                    case R.id.navigation_scoreboard:
+                        viewPager.setCurrentItem(3, true);
+                        break;
+                }
+                return true;
+            }
+        });
+
     }
 }
